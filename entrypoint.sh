e58a54b1600076d980d87c77e7ff639c5fef8936
Nathan Coad <nathan@cdc.home>
2017-11-16 20:49:15 +1100

    permissions hack for entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,9 @@
 cp -v /nginx.conf /etc/nginx/nginx.conf
 sed -i -e "s/PASSWORDTOKEN/${PASSWORD}/" /etc/nginx/nginx.conf
 
+# Adjust permissions for video files
+chmod 777 /hls
+chmod 777 /recordings
+
 # Start nginx
 nginx -g "daemon off;"
