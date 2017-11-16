8a72bc9d734b7076d300a545cff2e77c0b89ab24
Nathan Coad <nathan@cdc.home>
2017-11-16 18:43:24 +1100

    tweaks

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Set up config file
-cp /nginx.conf /etc/nginx/nginx.conf
-sed -i -e 's@PASSWORDTOKEN@'"${PASSWORD}"'@' /etc/nginx/nginx.conf
+cp -v /nginx.conf /etc/nginx/nginx.conf
+sed -i -e "s/PASSWORDTOKEN/${PASSWORD}/" /etc/nginx/nginx.conf
 
 # Start nginx
 nginx -g "daemon off;"
